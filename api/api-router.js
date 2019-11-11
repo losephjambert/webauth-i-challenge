d53d56fc1d56619888ccb46709d67d9f2be86f58
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-11 12:47:07 -0800

    scaffolds auth router

--- api/api-router.js
+++ api/api-router.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
+const authRouter = require('../auth/auth-router.js');
+
 router.get('/', (req, res) => {
   res.status(200).json({ api: `Now coming to you live from the api` });
 });
+
+router.use('/auth', authRouter);
 
 module.exports = router;
